e21cad274af539c553faf8195f5f21faf891ca27
mason <mason.esmailbegui@gmail.com>
2025-10-19 19:19:35 -0400

    leetcode 709 wip

---
+++ leetcode/0709_to_lowercase.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+#include <stdio.h>
+
+char* toLowerCase(char* s) {
+
+}
+ 
+void example_1() {
+printf("Example 1:\n");
+
+
+
+
+
+}
+
+
+
+void example_2() {
+printf("Example 2:\n");
+
+
+
+
+
+
+}

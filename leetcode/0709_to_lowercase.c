d1d98c1f30f5dabd2968d32431bc9110ff67bcb9
mason <mason.esmailbegui@gmail.com>
2025-10-19 21:48:35 -0400

    finished

--- leetcode/0709_to_lowercase.c
+++ leetcode/0709_to_lowercase.c
@@ -1,27 +1,51 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-char* toLowerCase(char* s) {
+// https://leetcode.com/problems/to-lower-case/description/
 
+char *toLowerCase(char *s) {
+
+  int len = strlen(s);
+//   printf("%s is of len %d\n", s, len);
+  for (int i = 0; i < len; i++) {
+    s[i] = tolower(s[i]);
+    // printf("Before %c\n", s[i]);
+    // if (s[i] >= 'A' && s[i] <= 'Z') {
+    //   s[i] = s[i] + ('a' - 'A');
+    // }
+    // printf("After %c\n", s[i]);
+  }
+  return s;
 }
- 
+
 void example_1() {
-printf("Example 1:\n");
-
-
-
-
-
+  printf("Example 1:\n");
+  char val[] = "Hello";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "hello") == 0);
 }
-
-
 
 void example_2() {
-printf("Example 2:\n");
+  printf("Example 2:\n");
+  char val[] = "here";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "here") == 0);
+}
 
+void example_3() {
+  printf("Example 3:\n");
+  char val[] = "LOVELY";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "lovely") == 0);
+}
 
+int main(int argc, char *argv[]) {
 
+  example_1();
+  example_2();
+  example_3();
 
-
-
+  return 0;
 }

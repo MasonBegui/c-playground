f4f261711f2ae459df881ff2c44ced32f1f615dd
Mason Begui <55628969+MasonBegui@users.noreply.github.com>
2025-10-21 21:09:09 -0400

    Leetcode/0709 (#2)
    * leetcode 709 
    
    * adding cstrings hello world example
    
    * working with cstrings

---
+++ leetcode/0709_to_lowercase.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// https://leetcode.com/problems/to-lower-case/description/
+
+char *toLowerCase(char *s) {
+
+  int len = strlen(s);
+  for (int i = 0; i < len; i++) {
+    s[i] = tolower(s[i]);
+    // if (s[i] >= 'A' && s[i] <= 'Z') {
+    //   s[i] = s[i] + ('a' - 'A');
+    // }
+  }
+  return s;
+}
+
+void example_1() {
+  printf("Example 1:\n");
+  char val[] = "Hello";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "hello") == 0);
+}
+
+void example_2() {
+  printf("Example 2:\n");
+  char val[] = "here";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "here") == 0);
+}
+
+void example_3() {
+  printf("Example 3:\n");
+  char val[] = "LOVELY";
+  char *rtn = toLowerCase(val);
+  assert(strcmp(rtn, "lovely") == 0);
+}
+
+int main(int argc, char *argv[]) {
+
+  example_1();
+  example_2();
+  example_3();
+
+  return 0;
+}

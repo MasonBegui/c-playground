950ccec7d2dbe4a24e7d9f037a613357b03b328e
mason <mason.esmailbegui@gmail.com>
2026-08-31 22:10:50 -0400

    final solution

--- leetcode/657_robot_return.c
+++ leetcode/657_robot_return.c
@@ -4,13 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool judgeCircle(char *moves) { return false; }
+bool judgeCircle(char *moves) { 
+  int row = 0;
+  int col = 0;
+  for(int i = 0; i < strlen(moves); i++){
+    const char dir = moves[i];
+    // printf("%c \n", dir);
+    if('U' == dir){
+      row++;
+    } else if ('D' == dir) {
+      row--;
+    }else if ('L' == dir) {
+      col--;
+    }else if('R' == dir) {
+      col++;
+    }else {
+      // Ignore me
+    } 
+  }
+    return (row == 0 && col == 0);
+ }
 
 void example_1();
 void example_2();
+void example_3();
 int main(int argc, char *argv[]) {
   example_1();
   example_2();
+  example_3();
   return EXIT_SUCCESS;
 }
 
@@ -22,4 +43,9 @@ void example_1() {
 void example_2() {
   bool result = judgeCircle("LL");
   assert(result == false);
+}
+
+void example_3(){
+  bool result = judgeCircle("UDLR");
+  assert(result == true);
 }
